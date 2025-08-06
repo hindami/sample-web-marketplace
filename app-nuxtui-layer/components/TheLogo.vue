0217216aeeaa8b0c85f1fb2649b678a24b1bfa07
hindami <hindami050294@gmail.com>
2025-08-06 14:10:21 +0700

    update logo component

--- app-nuxtui-layer/components/TheLogo.vue
+++ app-nuxtui-layer/components/TheLogo.vue
@@ -1,15 +1,9 @@
 <script setup lang="ts">
-  import siteMeta from '@/site'
-  const { title, logo } = siteMeta
+  import logo from '../../app/assets/images/SampleMall.svg'
 </script>
 <template>
   <ULink data-pg-name="Logo" class="flex items-center sm:flex-row" to="/">
-    <UIcon :name="logo" class="text-4xl" height="28px" />
-    <h6
-      class="text-primary-600 dark:text-primary-200 font-extrabold font-serif ml-2 mt-4 text-2xl"
-    >
-      {{ title }}
-    </h6>
+    <img :src="logo" alt="logo" />
   </ULink>
 </template>
 <style scoped></style>
