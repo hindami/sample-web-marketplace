c0aea70ac4bec2f8a5dfe2f55982b2c5036b7e73
hindami <hindami050294@gmail.com>
2025-08-07 11:47:00 +0700

    update BannerSlider component

--- app/features/homepage/components/BannerSlider.vue
+++ app/features/homepage/components/BannerSlider.vue
@@ -1,18 +1,21 @@
 <script setup lang="ts">
   import { Swiper, SwiperSlide } from 'swiper/vue'
   import type { Banner } from '../types'
+  import { getRandomHexColor } from '~/utils/colors'
 
-  // Import Swiper styles
   import 'swiper/css'
   import 'swiper/css/pagination'
   import 'swiper/css/navigation'
 
-  // Import modules
   import { Pagination, Autoplay } from 'swiper/modules'
 
   const props = defineProps<{
     banners: Banner[]
   }>()
+
+  const defaultImages = props.banners.map(() => {
+    return `https://placehold.co/190x190/${getRandomHexColor()}/FFF`
+  })
 </script>
 
 <script lang="ts">
@@ -33,20 +36,10 @@
     <!-- <slot v-for="(item, index) in items" :item="item" :index="index" /> -->
     <swiper-slide v-for="(banner, index) in props.banners" :key="index">
       <NuxtLink :to="banner.url" class="banner-slider-item__container">
-        <img :src="banner.image" :alt="'bnr-' + index" />
+        <!-- <img :src="banner.image" :alt="'bnr-' + index" /> -->
+        <img :src="defaultImages[index]" :alt="'bnr-' + index" />
       </NuxtLink>
     </swiper-slide>
-    <!-- <swiper-slide>2</swiper-slide>
-    <swiper-slide>3</swiper-slide> -->
-    <!-- <swiper-slide v-for="n in 5" :key="n">Slide {{ n }}</swiper-slide>
-    <swiper-slide v-for="n in 5" :key="n">Slide {{ n }}</swiper-slide> -->
-
-    <!-- Optional navigation buttons -->
-    <!-- <div class="swiper-button-prev" />
-    <div class="swiper-button-next" /> -->
-
-    <!-- Optional pagination -->
-    <!-- <div class="swiper-pagination" /> -->
   </swiper>
 </template>
 
@@ -70,7 +63,3 @@
     height: 496px;
   }
 </style>
-
-<!-- <template>
-  <h2>Banner Slider</h2>
-</template> -->
